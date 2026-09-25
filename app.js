3202b8c394147498998c732c6aa92c6762620c51
mhamodernharmonyalliance <mhamodernharmonyalliance@gmail.com>
2026-09-25 19:02:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ let adsgramReady = false;
 function initAdsgram() {
   if (typeof window.Adsgram === 'undefined') return false;
   try {
-    AdController = window.Adsgram.init({ blockId: "48760" });
+    AdController = window.Adsgram.init({ blockId: "0" });
     adsgramReady = true;
     return true;
   } catch (e) { return false; }
